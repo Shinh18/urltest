92019f3f769501bf350681270283a8f6e117837c
muskan <Shinh@123>
2020-09-24 18:30:26 -0400

    added ability to read file, find urls

--- index.js
+++ index.js
@@ -1,31 +1,41 @@
 #!/usr/bin/env node
 
 const chalk = require("chalk");
+const boxen = require("boxen");
 const fetch = require("node-fetch");
 const fs = require("fs");
 const path = require("path");
 
+const boxenOptions = {
+    padding: 1,
+    margin: 1,
+    borderStyle: "round",
+    borderColor: "blue",
+    backgroundColor: "#FFFFFF"
+}
+
+
+
+const version = boxen(chalk.redBright.bold("Urltester 1.0.0"), boxenOptions);
+
 if(process.argv.length == 2 ){
-    console.log("Standard user manual");
-    console.log("----------------------"); 
+    
+    console.log(chalk.red("\n                Standard user manual                "));
+    console.log((chalk.gray("----------------------------------------------------"))); 
+    console.log(chalk.redBright("urltester filename   -"), "reports good,bad,unknown urls"); 
+    console.log(chalk.redBright("urltester v |version -"), "displays tool version"); 
+    console.log((chalk.redBright("----------------------------------------------------\n"))); 
 }
 
 else {
     if(process.argv[2] === "v" || process.argv[2] === "version") {
-        console.log("Urltest 1.0.0");
-        console.log("works");
+        console.log(version);
     }
-
-    else {
-     
-        
+    else {      
         const filePath = path.join(__dirname,process.argv[2]);
-
-        //reading file 
- 
         fs.readFile(filePath,'utf-8', function(err, data) {
             if(err) {
-                console.log("Unsuccesful file read", err)
+                console.log(chalk.red("Unsuccesful to read file"), err)
             }
             else {
                 const urlArr = data.match(/(http|https)(:\/\/)([\w+\-&@`~#$%^*.=/?:]+)/gi);
