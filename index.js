c6b8c326377ed872d9f8c0a71fdc35f75a46dc69
Shinh18 <mshinh18@gmail.com>
2020-11-27 16:45:58 -0500

    add run.js file for e2e test

--- index.js
+++ index.js
@@ -4,49 +4,10 @@ const boxen = require('boxen');
 const boxenOptions = require('./style/style');
 const chalk = require('chalk');
 const path = require('path');
-//const yargs = require('yargs');
 const fileHandler = require('./utils/processFile');
 const { printManual } = require('./utils/printManual');
 
 const version = boxen(chalk.redBright.bold('Urltester 1.0.0'), boxenOptions);
-
-/*
-const argv = yargs
-  .usage('Usage: $0 [option]')
-  .alias('f', 'file')
-  .nargs('f', 1)
-  .describe('f', 'Load a file')
-  .demandOption(['f'])
-  .help('h')
-  .alias('h', 'help')
-  .alias('v', 'version')
-  .alias('V', 'version')
-  .alias('all')
-  .alias('good')
-  .alias('bad')
-  .alias('j', 'json')
-  .describe('all', 'display all URLs')
-  .describe('good', 'display good URLs')
-  .describe('bad', 'display bad URLs')
-  .describe('json', 'display output in JSON format')
-  .version(`${version}`)
-  .describe('version', 'display tool version').argv;
-
-if (argv.file) {
-  const filePath = path.join(__dirname, argv.file);
-  if (argv.json) {
-    fileHandler.processFile(filePath, true, 'none');
-  } else if (argv.all) {
-    fileHandler.processFile(filePath, false, argv.all);
-  } else {
-    fileHandler.processFile(filePath, false, 'none');
-  }
-}
-
-process.on('exit', function (code) {
-  return console.log(`About to exit with code ${code}`);
-});
-*/
 
 if (process.argv.length == 2 || process.argv[2] === '--help') console.log(printManual());
 else if (process.argv[2] === 'version' || process.argv[2] === '-v') console.log(version);
