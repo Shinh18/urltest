cd5cd98dcb6e9704ac19844c61366beab5fcd536
Shinh18 <mshinh18@gmail.com>
2020-11-27 16:59:05 -0500

    update e2e tests

--- test/e2e.test.js
+++ test/e2e.test.js
@@ -4,9 +4,8 @@ const { printManual } = require('../utils/printManual');
 
 describe('end-to-end integration', () => {
   test('when no arguments passed prints error and help message', async () => {
-    const { stderr, stdout, exitCode } = await run();
+    const { stdout, exitCode } = await run();
     expect(exitCode).toBe(0);
-    expect(stderr).toMatchSnapshot();
     expect(stdout).toEqual(printManual());
   });
 });
